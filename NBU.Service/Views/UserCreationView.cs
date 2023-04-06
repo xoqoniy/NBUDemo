75d388aa797819f48a3cc4bcda1dd175e77afd1e
Safarmurod7 <sm.ashurov7@gmail.com>
2023-04-06 12:26:22 +0500

    UserCreationView created

---
+++ NBU.Service/Views/UserCreationView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBU.Service.Views
+{
+	public class UserCreationView
+	{
+		[Required, MaxLength(40)]
+		[DisplayName("First Name")]
+		public string FirstName { get; set; }
+		[Required, MaxLength(40)]
+		[DisplayName("Last Name")]
+		public string LastName { get; set; }
+		[EmailAddress]
+		public string Email { get; set; }
+		[MinLength(4)]
+		public string Password { get; set; }
+		[Phone]
+		[DisplayName("Phone Number")]
+		public string PhoneNumber { get; set; }
+		[MinLength(3), MaxLength(15)]
+		[DisplayName("Username")]
+		public string UserName { get; set; }
+
+
+	}
+}

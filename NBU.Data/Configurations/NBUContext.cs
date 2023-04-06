3429ea492ebe4e9529edad93900bd09a24abbda6
Safarmurod7 <sm.ashurov7@gmail.com>
2023-04-06 11:27:06 +0500

    Database connected with Project

--- NBU.Data/Configurations/NBUContext.cs
+++ NBU.Data/Configurations/NBUContext.cs
@@ -6,12 +6,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace NBU.Data.Configurations
 {
 	public class NBUContext : DbContext
 	{
 		public NBUContext(DbContextOptions<NBUContext>options):base (options){ }
+		private readonly string connection = "Server=(localdb)\\mssqllocaldb; Database=NBUDb; Trusted_Connection=True; MultipleActiveResultSets=true";
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(connection, b => b.MigrationsAssembly("NBU"));
+		}
 		public DbSet<User> Users { get; set; }
 	}
 }
---
+++ NBU/Migrations/20230406062303_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NBU.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

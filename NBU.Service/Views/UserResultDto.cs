b7f93c2e6281c7c4244d93bc9b7775209c46450e
Safarmurod7 <sm.ashurov7@gmail.com>
2023-04-06 12:31:09 +0500

    UserResultDto has been created

--- NBU.Service/Views/UserResultDto.cs
+++ NBU.Service/Views/UserResultDto.cs
@@ -8,6 +8,12 @@ namespace NBU.Service.Views
 {
 	public class UserResultDto
 	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+		public string UserName { get; set; }
+
 
 	}
 }

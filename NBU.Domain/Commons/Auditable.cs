ad846d79129864079c7a9dafd8b71abca3162fe7
Safarmurod7 <sm.ashurov7@gmail.com>
2023-04-06 12:16:02 +0500

    Auditable Created

---
+++ NBU.Domain/Commons/Auditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBU.Domain.Commons
+{
+	public abstract class Auditable
+	{
+		public int Id { get; set; }
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+		public DateTime? UpdatedAt { get; set; }
+	}
+}

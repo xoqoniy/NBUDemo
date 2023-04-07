ae95c08e53f29e6f8a4e3ed35f90019d51ce1b13
Safarmurod7 <sm.ashurov7@gmail.com>
2023-04-07 11:31:15 +0500

    UserService readded

--- NBU.Service/Services/UserService.cs
+++ NBU.Service/Services/UserService.cs
@@ -21,29 +21,125 @@ namespace NBU.Service.Services
 			this.userRepository = userRepository;
 		}
 
-		public ValueTask<Response<User>> CreateUserAsync(UserCreationView user)
+		public async ValueTask<Response<User>> CreateUserAsync(UserCreationView user)
 		{
-			throw new NotImplementedException();
+			var person = await this.userRepository.GetUserByIdAsync(u => u.UserName.Equals(user.UserName));
+			if (person is not null)
+			{
+				return new Response<User>
+				{
+					StatusCode = 404,
+					Message = "Come on, User already exists",
+					Value = new User
+					{
+						FirstName = user.FirstName,
+						LastName = user.LastName,
+						PhoneNumber = user.PhoneNumber,
+						Email = user.Email,
+						UserName = user.UserName
+					}
+				};
+			}
+			var addedUser = await this.userRepository.InsertUserAsync(person);
+			var resultDto = new User
+			{
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				PhoneNumber = user.PhoneNumber,
+				Email = user.Email,
+				UserName = user.UserName,
+			};
+			return new Response<User>
+			{
+				StatusCode = 200,
+				Message = "Succesfully added to the database",
+				Value = resultDto
+			};
 		}
 
-		public ValueTask<Response<bool>> DeleteUserAsync(Predicate<User> predicate)
+		public async ValueTask<Response<bool>> DeleteUserAsync(Predicate<User> predicate)
 		{
-			throw new NotImplementedException();
+			User user = await this.userRepository.GetUserByIdAsync(user => predicate(user));
+			if (user is null)
+			{
+				return new Response<bool>
+				{
+					StatusCode = 404,
+					Message = "Could not find for given username",
+					Value = false
+				};
+			}
+			await this.userRepository.DeleteUserAsync(u => predicate(user));
+			return new Response<bool>
+			{
+				StatusCode = 200,
+				Message = "Successfully deleted",
+				Value = true
+			};
+
 		}
 
 		public Response<List<User>> GetAllUsers(Predicate<User> predicate = null)
 		{
-			throw new NotImplementedException();
+			var users = this.userRepository.GetAllUsers();
+			if (users.Any())
+			{
+				return new Response<List<User>>
+				{
+					StatusCode = 200,
+					Message = "Success",
+					Value = users.ToList()
+				};
+			}
+			return new Response<List<User>>
+			{
+				StatusCode = 404,
+				Message = "No date can be found",
+				Value = null
+			};
 		}
 
-		public ValueTask<Response<User>> GetbyIdAsync(Predicate<User> predicate)
+		public async ValueTask<Response<User>> GetbyIdAsync(Predicate<User> predicate)
 		{
-			throw new NotImplementedException();
+			User user = await this.userRepository.GetUserByIdAsync(user => predicate(user));
+			if (user is null)
+			{
+				return new Response<User>
+				{
+					StatusCode = 404,
+					Message = "User is not found",
+					Value = null
+				};
+			}
+			return new Response<User>
+			{
+				StatusCode = 200,
+				Message = "Here he/she is",
+				Value = user
+			};
+
 		}
 
-		public ValueTask<Response<User>> UpdateUserAsync(Predicate<User> predicate, User user)
+		public async ValueTask<Response<User>> UpdateUserAsync(Predicate<User> predicate, User user)
 		{
-			throw new NotImplementedException();
+			User person = await this.userRepository.GetUserByIdAsync(user => predicate(user));
+			if (person is null)
+			{
+				return new Response<User>
+				{
+					StatusCode = 201,
+					Message = "User couldn't be found for given Id",
+					Value = null
+				};
+			}
+			var updatedUser = await this.userRepository.UpdateUserAsync(user);
+
+			return new Response<User>
+			{
+				StatusCode = 200,
+				Message = "Success",
+				Value = updatedUser
+			};
 		}
 	}
 }
